d2643a09e1f9296096ca715ca5fb9d43c6f8e97f
tranphieu97 <33111131+tranphieu97@users.noreply.github.com>
2018-10-16 16:02:06 +0700

    Backlog 16 - 50%
    Hoàn thành khung sườn
    Hoàn thành giao diện Header, NavigationBar
    Chưa xử lý đóng mở Navigationbar

--- src/app/master-page/header/header.component.ts
+++ src/app/master-page/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MasterPageService } from '../../core/services/master-page.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private mtpService: MasterPageService) { }
 
   ngOnInit() {
   }
