d7d85f8b607d1e27ab9382a37fab79c3b6d21187
tranphieu97 <33111131+tranphieu97@users.noreply.github.com>
2018-11-06 14:22:12 +0700

    Change Model

--- src/app/model/Catagory.ts
+++ src/app/model/Catagory.ts
@@ -1,12 +1,10 @@
 export class Catagory {
     public Name: string;
-    public Image: string;
     public Link: string;
     public Tags: Array<string>;
 
-    constructor(name: string, img: string, link: string, tags: Array<string>) {
+    constructor(name: string, link: string, tags: Array<string>) {
         this.Name = name;
-        this.Image = img;
         this.Link = link;
         this.Tags = tags;
     }
