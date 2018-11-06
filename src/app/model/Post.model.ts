dacfedc0e647926ca82b67a66ea9a893e82eac1c
minhthong8197 <minhthong8197@gmail.com>
2018-11-06 15:14:48 +0700

    added models for Post

---
+++ src/app/model/Post.model.ts
@@ -0,0 +1,20 @@
+import { PostContent } from './postContent.model';
+import { Comment } from './comment.model';
+
+export class Post {
+    constructor(
+        public id: number,
+        public createdTime: Date,
+        public approvedTime: Date,
+        public tag: Array<string>,
+        public postContents: Array<PostContent>,
+        public title: string,
+        public cover: string,
+        public authorId: number,
+        public location: any,
+        public categories: string,
+        public rating: number,
+        public status: string,
+        public comments: Array<Comment>,
+        public description: string) { }
+}
---
+++ src/app/model/PostContent.model.ts
@@ -0,0 +1,11 @@
+export class PostContent {
+    public title: string;
+    public content: string;
+    public image: string;
+
+    constructor(title: string, content: string, image: string) {
+        this.title = title;
+        this.content = content;
+        this.image = image;
+    }
+}
