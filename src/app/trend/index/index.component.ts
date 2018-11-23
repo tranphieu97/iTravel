7e78c3b5ce53541b5d632f33dc5f1a60cd66a979
tranphieu97 <33111131+tranphieu97@users.noreply.github.com>
2018-11-23 08:49:48 +0700

    commit temp

--- src/app/trend/index/index.component.ts
+++ src/app/trend/index/index.component.ts
@@ -22,9 +22,51 @@ export class IndexComponent implements OnInit, OnDestroy {
     this.pieChartEndDate = new Date(Date.now());
     this.pieChartStartDate = new Date(this.pieChartEndDate.getFullYear(),
             this.pieChartEndDate.getMonth(), this.pieChartEndDate.getDay() - 7);
-    this.server.getReportBySearchKeyWordData(this.pieChartStartDate, this.pieChartEndDate).subscribe((result) => {
+    // this.server.getReportBySearchKeyWordData(this.pieChartStartDate, this.pieChartEndDate).subscribe((result) => {
 
-      this.createPieChart(this.pieChart, 'pie-chart-search', []);
+    // }
+
+    this.pieChart = this.amchartServices.makeChart('pie-chart-search', {
+      'type': 'pie',
+      'theme': 'light',
+      'dataProvider': [{
+        'country': 'Lithuania',
+        'litres': 501.9
+      }, {
+        'country': 'Czech Republic',
+        'litres': 301.9
+      }, {
+        'country': 'Ireland',
+        'litres': 201.1
+      }, {
+        'country': 'Germany',
+        'litres': 165.8
+      }, {
+        'country': 'Australia',
+        'litres': 139.9
+      }, {
+        'country': 'Austria',
+        'litres': 128.3
+      }, {
+        'country': 'UK',
+        'litres': 99
+      }, {
+        'country': 'Belgium',
+        'litres': 60
+      }, {
+        'country': 'The Netherlands',
+        'litres': 50
+      }],
+      'titleField': 'title',
+      'valueField': 'value',
+
+      'labelRadius': 5,
+      'radius': '42%',
+      'innerRadius': '60%',
+      'labelText': '[[title]]',
+      'export': {
+        'enabled': true
+      }
     });
   }
 
@@ -34,21 +76,8 @@ export class IndexComponent implements OnInit, OnDestroy {
     }
   }
 
-  createPieChart(chart: AmChart, divID: string, data: any) {
-    chart = this.amchartServices.makeChart(divID, {
-      'type': 'pie',
-      'theme': 'light',
-      'dataProvider': data,
-      'titleField': 'title',
-      'valueField': 'value',
-      'labelRadius': 5,
-      'radius': '42%',
-      'innerRadius': '60%',
-      'labelText': '[[title]]',
-      'export': {
-        'enabled': true
-      }
-    });
+  createPieChart(chart: AmChart, divID: string) {
+    
   }
 
   destroyChart(chart: AmChart) {
