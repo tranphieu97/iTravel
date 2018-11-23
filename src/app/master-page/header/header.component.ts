f90ed7096815f995b1633a58d0b5fb97e992f347
tranphieu97 <33111131+tranphieu97@users.noreply.github.com>
2018-11-23 13:41:51 +0700

    Fixed Header dropdown - 40%

--- src/app/master-page/header/header.component.ts
+++ src/app/master-page/header/header.component.ts
@@ -3,6 +3,7 @@ import { MasterPageService } from '../../core/services/master-page.service';
 import { SharedModule } from '../../shared/shared.module';
 import { ServerService } from '../../core/services/server.service';
 import { SearchHistory } from 'src/app/model/searchHistory.model';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -14,9 +15,18 @@ export class HeaderComponent implements OnInit {
   isShowSearchBox: Boolean = false;
   searchBoxText: String = '';
 
+  headerFlags = {
+    isShowSearchBox: false,
+    isShowNotification: false,
+    isShowLanguage: false,
+    isShowSetting: false,
+    isShowUserTool: false
+  };
+
   constructor(private mtpService: MasterPageService, private server: ServerService) { }
 
   ngOnInit() {
+
   }
 
   searchByText() {
@@ -26,6 +36,23 @@ export class HeaderComponent implements OnInit {
       this.server.postSearchHistory(searchHistory).subscribe();
       this.searchBoxText = '';
       this.isShowSearchBox = false;
+
+      this.changeHeaderFlagByFlagName('isShowSearchBox');
+    }
+  }
+
+  changeHeaderFlagByFlagName(flagName: string): void {
+    if (this.headerFlags[flagName]) {
+      this.headerFlags[flagName] = false;
+    } else {
+      const listProperties = Object.getOwnPropertyNames(this.headerFlags);
+
+      listProperties.forEach(property => {
+        this.headerFlags[property.toString()] = false;
+      });
+      if (listProperties.includes(flagName)) {
+        this.headerFlags[flagName] = true;
+      }
     }
   }
 }
