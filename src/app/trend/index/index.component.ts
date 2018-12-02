80ca5e0e409e22d459995c871115abaeef5cd15b
tranphieu97 <33111131+tranphieu97@users.noreply.github.com>
2018-12-02 15:58:30 +0700

    Trend Module - Clean code - 100%

--- src/app/trend/index/index.component.ts
+++ src/app/trend/index/index.component.ts
@@ -23,50 +23,53 @@ export class IndexComponent implements OnInit, OnDestroy {
     this.pieChartEndDate = new Date(Date.now());
     this.pieChartStartDate = new Date(this.pieChartEndDate.getFullYear(),
       this.pieChartEndDate.getMonth(), this.pieChartEndDate.getDay() - 7);
+
     this.server.getReportBySearchKeyWordData(this.pieChartStartDate, this.pieChartEndDate)
       .subscribe((result) => {
         const searchData = result.data;
-        let searchDataLength = searchData.length;
 
-        // Calculator how many time search a key word
-        for (let index = 0; index < searchDataLength; index++) {
-          const record = searchData[index];
-
-          for (let indexSecondLoop = index + 1; indexSecondLoop < searchDataLength; indexSecondLoop++) {
-            const diffRecord = searchData[indexSecondLoop];
-
-            if (record.keyword === diffRecord.keyword) {
-              record.count++;
-              searchData.splice(indexSecondLoop, 1);
-              searchDataLength--;
-              indexSecondLoop--;
-            }
-          }
-          record.keyword = record.keyword.toUpperCase();
-          searchData[index] = record;
-        }
-
-        // Sort by counter
-        searchData.sort((n1, n2) => {
-          return n2.count - n1.count;
-        });
-
-        // Get top 10
-        if (searchData.length > 10) {
-          const otherRecord = searchData[9];
-          otherRecord.keyword = 'The Others';
-          for (let index = 10; index < searchDataLength; index++) {
-            const record = searchData[index];
-            otherRecord.count += record.count;
-          }
-          searchData.splice(10, searchData.length - 10);
-        }
-
+        this.getTop10SearchData(searchData);
         this.createPieChart(this.pieChart, 'piechart-search', searchData);
       });
   }
 
+  getTop10SearchData(arrData: any) {
+    let searchDataLength = arrData.length;
 
+    // Calculator how many time search a key word
+    for (let index = 0; index < searchDataLength; index++) {
+      const record = arrData[index];
+
+      for (let indexSecondLoop = index + 1; indexSecondLoop < searchDataLength; indexSecondLoop++) {
+        const diffRecord = arrData[indexSecondLoop];
+
+        if (record.keyword === diffRecord.keyword) {
+          record.count++;
+          arrData.splice(indexSecondLoop, 1);
+          searchDataLength--;
+          indexSecondLoop--;
+        }
+      }
+      record.keyword = record.keyword.toUpperCase();
+      arrData[index] = record;
+    }
+
+    // Sort by counter
+    arrData.sort((n1, n2) => {
+      return n2.count - n1.count;
+    });
+
+    // Get top 10
+    if (arrData.length > 10) {
+      const otherRecord = arrData[9];
+      otherRecord.keyword = 'The Others';
+      for (let index = 10; index < searchDataLength; index++) {
+        const record = arrData[index];
+        otherRecord.count += record.count;
+      }
+      arrData.splice(10, arrData.length - 10);
+    }
+  }
 
   ngOnDestroy(): void {
     if (this.pieChart) {
