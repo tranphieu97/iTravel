7aeb7bfa6a87707912f674c53e89d75bdc302e84
minhthong8197 <minhthong8197@gmail.com>
2019-05-20 00:18:32 +0700

    done tab member

--- src/app/manager/tour-management/detail-modal/tour-member/tour-member.component.ts
+++ src/app/manager/tour-management/detail-modal/tour-member/tour-member.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { Tour } from 'src/app/model/tour.model';
+import { ServerService } from 'src/app/core/services/server.service';
+import { TourMember } from 'src/app/model/tour-member.model';
 
 @Component({
   selector: 'app-tour-member',
   templateUrl: './tour-member.component.html',
   styleUrls: ['./tour-member.component.scss']
 })
-export class TourMemberComponent implements OnInit {
+export class TourMemberComponent implements OnInit, AfterViewInit {
   @Input() tourData: Tour;
+  tourMemberInfo: {
+    tourInfo: TourMember;
+    basicInfo: { firstName: string; lastName: string; avatar: string };
+  }[];
+  available = 0;
 
-  constructor() {}
+  constructor(private serverService: ServerService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.available = this.tourData.memberLimit - this.tourData.members.length;
+    this.tourMemberInfo = this.tourData.members.map(member => ({
+      tourInfo: member,
+      basicInfo: { firstName: '', lastName: '', avatar: '' }
+    }));
+  }
+
+  ngAfterViewInit() {
+    this.tourMemberInfo.forEach((info, index) => {
+      this.serverService
+        .getUserBasicInfo(info.tourInfo.memberId)
+        .subscribe(res => {
+          if (res.data) {
+            this.tourMemberInfo[index].basicInfo = res.data;
+          }
+        });
+    });
+  }
 }
