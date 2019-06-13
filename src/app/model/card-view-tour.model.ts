217ad923b797ebce730898dd540d5928e891859d
tranphieu97 <33111131+tranphieu97@users.noreply.github.com>
2019-06-13 09:26:44 +0700

    Fix model make build error

--- src/app/model/card-view-tour.model.ts
+++ src/app/model/card-view-tour.model.ts
@@ -10,19 +10,21 @@ export class CardViewTour {
     public closeFeedbackTime: Date;
     public closeRegisterTime: Date;
     public durationTime: number;
+    public description: string;
 
     constructor(_id: string, tourName: string, registerCost: number, locationIds: Array<string>, beginTime: Date,
-        endTime: Date, cover: string, status: string, closeFeedbackTime: Date, closeRegisterTime: Date, durationTime: number) {
-            this._id = _id;
-            this.tourName = tourName;
-            this.registerCost = registerCost;
-            this.locationIds = locationIds;
-            this.beginTime = beginTime;
-            this.endTime = endTime;
-            this.cover = cover;
-            this.status = status;
-            this.closeFeedbackTime = closeFeedbackTime;
-            this.closeRegisterTime = closeRegisterTime;
-            this.durationTime = durationTime;
-        }
+        endTime: Date, cover: string, status: string, closeFeedbackTime: Date, closeRegisterTime: Date, durationTime: number, description: string) {
+        this._id = _id;
+        this.tourName = tourName;
+        this.registerCost = registerCost;
+        this.locationIds = locationIds;
+        this.beginTime = beginTime;
+        this.endTime = endTime;
+        this.cover = cover;
+        this.status = status;
+        this.closeFeedbackTime = closeFeedbackTime;
+        this.closeRegisterTime = closeRegisterTime;
+        this.durationTime = durationTime;
+        this.description = description;
+    }
 }
