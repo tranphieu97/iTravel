121707db17742f5ff25010d64153ea6eb1274f6b
minhthong8197 <minhthong8197@gmail.com>
2019-06-28 19:39:08 +0700

    fix bug
    - fix bug noti lấy ngược
    - fix hiện chi phí trong schedule ở status khác pending
    - fix giao diện member
    - them link to khi bam noti

--- src/app/master-page/notification/notification.component.ts
+++ src/app/master-page/notification/notification.component.ts
@@ -4,6 +4,7 @@ import { UserService } from 'src/app/core/services/user.service';
 import { NotificationItem } from 'src/app/model/notification-item.model';
 import { Notification } from 'src/app/model/notification.model';
 import { LanguageService } from '../../core/services/language.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notification',
@@ -11,15 +12,19 @@ import { LanguageService } from '../../core/services/language.service';
   styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
-
   // @Input() public inputNotification: Notification;
   // public listNotification: Array<Notification> = [];
   public notificationItems: Array<NotificationItem> = [];
-  public recentNotificationItems: Array<Notification> = [];
+  public recentNotificationItems: Array<NotificationItem> = [];
   sendTime: string;
   sendDate: string;
   compLanguage;
-  constructor(public language: LanguageService, private serverService: ServerService, public userService: UserService) { }
+  constructor(
+    public language: LanguageService,
+    private serverService: ServerService,
+    public userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     if (this.userService.isLogin) {
@@ -30,39 +35,64 @@ export class NotificationComponent implements OnInit {
         this.fetchNotification();
       }
     });
+    this.userService.hasChangeUser.asObservable().subscribe(() => {
+      if (!this.userService.isLogin) {
+        this.notificationItems = [];
+        this.recentNotificationItems = [];
+      }
+    });
     this.compLanguage = this.language.currentLanguage.compNotification;
-    this.language.hasChangeLanguage.subscribe(() => this.compLanguage = this.language.currentLanguage.compNotification);
+    this.language.hasChangeLanguage.subscribe(
+      () => (this.compLanguage = this.language.currentLanguage.compNotification)
+    );
   }
 
   fetchNotification() {
-    this.serverService.getUserNotification(this.userService.currentUser._id).subscribe(receiveData => {
-      if (receiveData.data) {
-        this.notificationItems = receiveData.data.notificationItems;
-        this.recentNotificationItems = receiveData.data.notificationItems.slice(0, 10);
-      } else {
-        // if current user not has notification, create an empty one
-        const newNoti = new Notification(this.userService.currentUser._id, []);
-        this.serverService.postNewNotification(newNoti).subscribe();
-      }
-    });
+    this.serverService
+      .getUserNotification(this.userService.currentUser._id)
+      .subscribe(receiveData => {
+        if (receiveData.data) {
+          this.notificationItems = [...receiveData.data.notificationItems];
+          this.recentNotificationItems = [...this.notificationItems]
+            .reverse()
+            .slice(0, 10);
+        } else {
+          // if current user not has notification, create an empty one
+          const newNoti = new Notification(
+            this.userService.currentUser._id,
+            []
+          );
+          this.serverService.postNewNotification(newNoti).subscribe();
+        }
+      });
   }
 
-  handleShowContent(divContent: HTMLDivElement, divShortContent: HTMLDivElement) {
+  handleShowContent(
+    divContent: HTMLDivElement,
+    divShortContent: HTMLDivElement
+  ) {
     divContent.classList.add('show');
     divShortContent.classList.remove('show');
   }
 
-  handleHideContent(divContent: HTMLDivElement, divShortContent: HTMLDivElement) {
+  handleHideContent(
+    divContent: HTMLDivElement,
+    divShortContent: HTMLDivElement
+  ) {
     divContent.classList.remove('show');
     divShortContent.classList.add('show');
   }
 
   handleSeeNotification(noti: NotificationItem) {
-    const foundNoti = this.notificationItems.find(eachNoti => eachNoti._id === noti._id);
+    const foundNoti = this.notificationItems.find(
+      eachNoti => eachNoti._id === noti._id
+    );
     foundNoti.seen = true;
-    // foundNoti.seen = !foundNoti.seen;
-    this.serverService.updateNotification(this.notificationItems).subscribe((resData) => {
-    });
+    this.serverService.updateNotification(this.notificationItems).subscribe();
+    // navigate
+    if (noti.linkTo) {
+      this.router.navigate([noti.linkTo]);
+    }
   }
 
   getTime(notifyItem: NotificationItem) {
